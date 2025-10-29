c491c2ef865450521d5f674333c36fd865c75072
TapanutAkk <tapanut.akk@gmail.com>
2025-10-29 15:39:10 +0700

    seed super user

--- management_api/migrations/0002_create_user.py
+++ management_api/migrations/0002_create_user.py
@@ -6,6 +6,23 @@ def create_normal_user(apps, schema_editor):
     """
     User = apps.get_model('auth', 'User')
 
+    ADMIN_USER_USERNAME = 'admin'
+    ADMIN_USER_PASSWORD = 'AdminPassword123'
+    ADMIN_USER_EMAIL = 'admin@example.com'
+
+    try:
+        user = User.objects.get(username=ADMIN_USER_USERNAME)
+        print(f"\nUser '{ADMIN_USER_USERNAME}' already exists.")
+    except User.DoesNotExist:
+        user = User.objects.create_user(
+            username=ADMIN_USER_USERNAME,
+            email=ADMIN_USER_EMAIL,
+            password=ADMIN_USER_PASSWORD,
+            is_staff=True,
+            is_superuser=True
+        )
+        print(f"\nCreated Super User: {ADMIN_USER_EMAIL} (Staff Status: True)") 
+    
     NORMAL_USER_USERNAME = 'tester'
     NORMAL_USER_PASSWORD = 'TestPassword123'
     NORMAL_USER_EMAIL = 'tester@example.com'
@@ -26,6 +43,8 @@ def create_normal_user(apps, schema_editor):
 def reverse_create_normal_user(apps, schema_editor):
     """ฟังก์ชันสำหรับลบ User หากมีการย้อน Migration"""
     User = apps.get_model('auth', 'User')
+    User.objects.filter(username='admin').delete()
+    print("\nDeleted Super User: tester")
     User.objects.filter(username='tester').delete()
     print("\nDeleted Normal User: tester")
 
